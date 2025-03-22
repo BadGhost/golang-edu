7e1db06b6391bd3e11e37dd89507d7bc08e1f9af
iskandar <zulqarnainf26@gmail.com>
2025-03-22 21:46:44 +0800

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "flag"
+    "log/slog"
+    "net/http"
+    "os"
+)
+
+// Define an application struct to hold the application-wide dependencies for the
+// web application. For now we'll only include the structured logger, but we'll
+// add more to this as the build progresses.
+type application struct {
+    logger *slog.Logger
+}
+
+func main() {
+    addr := flag.String("addr", ":4000", "HTTP network address")
+    flag.Parse()
+
+    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+    app := &application{
+        logger: logger,
+    }
+
+    logger.Info("starting server", "addr", *addr)
+    
+    // Call the new app.routes() method to get the servemux containing our routes,
+    // and pass that to http.ListenAndServe().
+    err := http.ListenAndServe(*addr, app.routes())
+    logger.Error(err.Error())
+    os.Exit(1)
+}
