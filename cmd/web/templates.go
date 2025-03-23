f48a1d2a2559192cc633f06021ce0e7df33d8f1a
iskandar <zulqarnainf26@gmail.com>
2025-03-23 17:37:55 +0800

    refactor template handling to use .html extensions and update rendering logic with buffered output

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -21,7 +21,7 @@ type templateData struct {
 func newTemplateCache() (map[string]*template.Template, error) {
     cache := map[string]*template.Template{}
 
-    pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+    pages, err := filepath.Glob("./ui/html/pages/*.html")
     if err != nil {
         return nil, err
     }
@@ -30,13 +30,13 @@ func newTemplateCache() (map[string]*template.Template, error) {
         name := filepath.Base(page)
 
         // Parse the base template file into a template set.
-        ts, err := template.ParseFiles("./ui/html/base.tmpl")
+        ts, err := template.ParseFiles("./ui/html/base.html")
         if err != nil {
             return nil, err
         }
 
         // Call ParseGlob() *on this template set* to add any partials.
-        ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+        ts, err = ts.ParseGlob("./ui/html/partials/*.html")
         if err != nil {
             return nil, err
         }
