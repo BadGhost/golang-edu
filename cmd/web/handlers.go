1fa3afcd02e4de69887c60125290445060b0f852
iskandar <zulqarnainf26@gmail.com>
2025-03-23 15:43:43 +0800

    implement snippet view functionality with HTML templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,7 @@ package main
 import (
     "errors" // New import
     "fmt"
-    // "html/template"
+    "html/template"
     "net/http"
     "strconv"
 
@@ -50,9 +50,6 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    // Use the SnippetModel's Get() method to retrieve the data for a
-    // specific record based on its ID. If no matching record is found,
-    // return a 404 Not Found response.
     snippet, err := app.snippets.Get(id)
     if err != nil {
         if errors.Is(err, models.ErrNoRecord) {
@@ -63,8 +60,27 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    // Write the snippet data as a plain-text HTTP response body.
-    fmt.Fprintf(w, "%+v", snippet)
+    // Initialize a slice containing the paths to the view.tmpl file,
+    // plus the base layout and navigation partial that we made earlier.
+    files := []string{
+        "./ui/html/base.html",
+        "./ui/html/partials/nav.html",
+        "./ui/html/pages/view.html",
+    }
+
+    // Parse the template files...
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        app.serverError(w, r, err)
+        return
+    }
+
+    // And then execute them. Notice how we are passing in the snippet
+    // data (a models.Snippet struct) as the final parameter?
+    err = ts.ExecuteTemplate(w, "base", snippet)
+    if err != nil {
+        app.serverError(w, r, err)
+    }
 }
 
 // Change the signature of the snippetCreate handler so it is defined as a method
